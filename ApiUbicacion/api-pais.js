22eb3a3a731dcc79d8e15e95f7c393df0721d907
Milton Jaimes <20193tn028@utez.edu.mx>
2023-10-25 08:51:36 -0600

    todo lo que se hizo el martes

--- ApiUbicacion/api-pais.js
+++ ApiUbicacion/api-pais.js
@@ -4,23 +4,31 @@ const baseUrl = "https://restcountries.com/v3.1/";
 // Endpoint para obtener la lista de todos los países
 const endpoint = "all";
 
-// Función para agregar opciones al select
-function addCountryToSelect(countryName) {
-  const countrySelect = document.getElementById("country-select");
+// Función para agregar opciones al datalist
+function addCountryToDatalist(countryName) {
+  const countriesDatalist = document.getElementById("countries");
   const option = document.createElement("option");
   option.value = countryName;
-  option.textContent = countryName;
-  countrySelect.appendChild(option);
+  countriesDatalist.appendChild(option);
 }
 
 // Realiza una solicitud GET a la API
 fetch(baseUrl + endpoint)
   .then(response => response.json())
   .then(data => {
-    // Obtén solo los nombres de los países y agrégalos al select
-    data.forEach(country => {
-      const countryName = country.name.common;
-      addCountryToSelect(countryName);
+    // Obtén solo los nombres de los países
+    const countryNames = data.map(country => country.name.common);
+    
+    // Ordena la lista de nombres de países alfabéticamente
+    countryNames.sort();
+
+    // Limpia el datalist actual
+    const countriesDatalist = document.getElementById("countries");
+    countriesDatalist.innerHTML = '';
+
+    // Agrega los nombres de países ordenados al datalist
+    countryNames.forEach(countryName => {
+      addCountryToDatalist(countryName);
     });
   })
   .catch(error => {
